9d536ba9496252808417c2023fb13a27e8c92351
jroiceee <jroicealdeza@gmail.com>
2022-04-16 16:29:26 +0800

    disable buttons to avoid overflow

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function numberButtonListener() {
-    const buttons = document.querySelectorAll(".number-button");
+function numberButtonListener(buttons) {
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
-           const numValue = getDataAttribute(button);
+            disableButtons(disable,buttons);
+            const numValue = getDataAttribute(button);
         })
     })
 }
 
 function getDataAttribute(button) {
+    if (counter > 20) {
+        disable = true;
+    }
+    counter++;
     const key = button.getAttribute("data-key");
     if (key == ".") {
         button.disabled = true;
@@ -21,7 +25,6 @@ function appendInput(key) {
     arr1.push(key);
     const newInput = arr1.join("");
     displayInput(newInput);
-    counter++;
 }
 
 function displayInput(newInput) {
@@ -29,10 +32,18 @@ function displayInput(newInput) {
    text.textContent = newInput;
 }
 
-numberButtonListener();
+function disableButtons(disable,buttons) {
+    if (disable) {
+        buttons.forEach((button) => {
+            button.disabled = true});
+        } 
+    }
 
+
+const buttons = document.querySelectorAll(".number-button");
 let arr1 = [];
 let arr2 = [];
 let arr3 = [];
 let counter = 0;
-const decimalDisabled = false;
+let disable = false;
+numberButtonListener(buttons);
