b5775ebcffd7fdab4ec02726037bf83a6c1b0b4e
jroiceee <jroicealdeza@gmail.com>
2022-04-16 16:42:45 +0800

    added disable button for operations

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function numberButtonListener(buttons) {
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
-            disableButtons(disable,buttons);
+            disableButtons(disable,buttons, operationButtons);
             const numValue = getDataAttribute(button);
+        })
+    })
+}
+function operationButtonListener(operationButtons) {
+    operationButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            disableButtons(disable,buttons, operationButtons);
+            const operatorValue = getDataAttribute(button);
         })
     })
 }
@@ -32,18 +40,20 @@ function displayInput(newInput) {
    text.textContent = newInput;
 }
 
-function disableButtons(disable,buttons) {
+function disableButtons(disable,buttons,operationButtons) {
     if (disable) {
-        buttons.forEach((button) => {
-            button.disabled = true});
+        buttons.forEach((button) => button.disabled = true);
+        operationButtons.forEach((button) => button.disabled = true);
         } 
     }
 
 
 const buttons = document.querySelectorAll(".number-button");
+const operationButtons = document.querySelectorAll(".smaller-button.operation-button")
 let arr1 = [];
 let arr2 = [];
 let arr3 = [];
 let counter = 0;
 let disable = false;
 numberButtonListener(buttons);
+operationButtonListener(operationButtons);
